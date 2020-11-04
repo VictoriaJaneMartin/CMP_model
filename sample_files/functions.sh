451c4f9c427328d61f0c23a496cbcc8a3ea8b4e2
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2020-11-04 15:37:43 +0000

    Reorganised functions

--- sample_files/functions.sh
+++ sample_files/functions.sh
@@ -4,11 +4,19 @@ alias asetup_gen="asetup 19.2.5.33.4,MCProd,64,here --cmtconfig=x86_64-slc6-gcc4
 alias asetup_sim="asetup 21.0.108,Athena,here"
 alias asetup_reco=$asetup_sim
 
-# Shortcut functions
 export workingDir="/afs/cern.ch/user/l/lprincip/public/workingDM"
-alias generate="source $workingDir/gen/generate.sh $*"
-alias simulate="source $workingDir/simReco/simulate.sh $*"
-alias reco="source $workingDir/simReco/reco.sh $*"
-alias analysis="athena Analysis/AnalysisAlgJobOptions.py $*"
 
+
+# Shortcut functions
+# for generation
+alias generate="source $workingDir/gen/generate.sh $*"
+
+# for simulation
+alias simulate="source $workingDir/simReco/simulate.sh $*"
+
+# for reconstruction
+alias reco="source $workingDir/simReco/reco.sh $*"
+
+# for analysis
+alias analysis="athena Analysis/AnalysisAlgJobOptions.py $*"
 alias buildAnalysis="acm compile && analysis $*"
