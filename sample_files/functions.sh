fc2075ad75853919af7acfb0188a3df1ae69daf6
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2020-11-04 10:58:47 +0000

    Reorganised sample functions file

--- sample_files/functions.sh
+++ sample_files/functions.sh
@@ -1,28 +1,14 @@
 # Defining shortcut functions
+# Used aliases
 alias asetup_gen="asetup 19.2.5.33.4,MCProd,64,here --cmtconfig=x86_64-slc6-gcc47-opt"
-alias generate="source /afs/cern.ch/user/l/lprincip/public/workingDM/gen/generate.sh $*"
 alias asetup_sim="asetup 21.0.108,Athena,here"
-alias simulate="source /afs/cern.ch/user/l/lprincip/public/workingDM/simReco/simulate.sh $*"
 alias asetup_reco=$asetup_sim
-alias reco="source /afs/cern.ch/user/l/lprincip/public/workingDM/simReco/reco.sh $*"
+
+# Shortcut functions
+export workingDir="/afs/cern.ch/user/l/lprincip/public/workingDM"
+alias generate="source $workingDir/gen/generate.sh $*"
+alias simulate="source $workingDir/simReco/simulate.sh $*"
+alias reco="source $workingDir/simReco/reco.sh $*"
 alias analysis="athena Analysis/AnalysisAlgJobOptions.py $*"
 
-toCernbox(){
-    if [[ $# -lt 1 ]]; then
-        echo "Usage: ${FUNCNAME[0]} <file to copy> [<other files to copy>]"
-        return 1
-    fi
-
-    for item in $*; do
-        cp -r $item $CERNBOX/autoCp/
-
-        if [[ $? -eq 0 ]]; then
-            echo "Copied $item to $CERNBOX/autoCP"
-        fi
-    done
-}
-
-buildAnalysis(){
-  acm compile && analysis $*
-        
-}
+alias buildAnalysis="acm compile && analysis $*"
