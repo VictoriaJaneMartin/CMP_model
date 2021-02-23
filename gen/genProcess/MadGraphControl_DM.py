da50e6f061dc061eb8aee867ea0bb2b738ce899c
joshdijkstra <74965856+joshdijkstra@users.noreply.github.com>
2021-02-23 12:11:33 +0000

    Update MadGraphControl_DM.py

--- gen/genProcess/MadGraphControl_DM.py
+++ gen/genProcess/MadGraphControl_DM.py
@@ -23,7 +23,7 @@ else:
     raise RuntimeError("runNumber %i not recognised in these jobOptions."%runArgs.runNumber)
 
 ######
-mgproc="generate p p > ~chi+ ~chi-, (~chi+ > ~Phi+ ~Psi), (~chi- > ~Phi- ~Psi~)"
+mgproc="generate p p > ~chi+ ~chi-, (~chi+ > ~phi+ ~psi), (~chi- > ~phi- ~psi~)"
 name='DM'
 process="pp>DM"
 
@@ -144,8 +144,8 @@ bonus_file = open('pdg_extras.dat','w')
 bonus_file.write('200001 ~Chi+ 200.0 (GeV/c) fermion ~Chi+ 0\n')
 bonus_file.write('-200001 ~Chi- 200.0 (GeV/c) fermion ~Chi- 0\n')
 
-bonus_file.write('5000001 ~Phi+ 65.0 (GeV/c) scalar ~Phi+ 0\n')
-bonus_file.write('-5000001 ~Phi- 65.0 (GeV/c) scalar ~Phi- 0\n')
+bonus_file.write('5000001 ~phi+ 65.0 (GeV/c) scalar ~phi+ 0\n')
+bonus_file.write('-5000001 ~phi- 65.0 (GeV/c) scalar ~phi- 0\n')
 
 bonus_file.write('200000 ~Psi+ 65.0 (GeV/c) fermion ~Psi+ 0\n')
 bonus_file.write('-200000 ~Psi- 65.0 (GeV/c) fermion ~Psi- 0\n')
