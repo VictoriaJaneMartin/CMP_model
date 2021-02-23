e03f0f870360519182ad77e3f86b48b014f0fcb4
joshdijkstra <74965856+joshdijkstra@users.noreply.github.com>
2021-02-23 12:39:19 +0000

    Update MadGraphControl_DM.py

--- gen/genProcess/MadGraphControl_DM.py
+++ gen/genProcess/MadGraphControl_DM.py
@@ -141,6 +141,7 @@ include("MC15JobOptions/Pythia8_MadGraph.py")
 
 # Chinew is non-interacting
 bonus_file = open('pdg_extras.dat','w')
+"""
 bonus_file.write('200001 ~Chi+ 200.0 (GeV/c) fermion ~Chi+ 0\n')
 bonus_file.write('-200001 ~Chi- 200.0 (GeV/c) fermion ~Chi- 0\n')
 
@@ -149,7 +150,7 @@ bonus_file.write('-5000001 ~phi- 65.0 (GeV/c) scalar ~phi- 0\n')
 
 bonus_file.write('200000 ~Psi+ 65.0 (GeV/c) fermion ~Psi+ 0\n')
 bonus_file.write('-200000 ~Psi- 65.0 (GeV/c) fermion ~Psi- 0\n')
-
+"""
 
 
 bonus_file.close()
