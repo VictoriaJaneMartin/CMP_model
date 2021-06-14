d979b0fe473e13c2b8d9af1f1db42b7aff9eb1d9
joshdijkstra <74965856+joshdijkstra@users.noreply.github.com>
2021-06-14 16:09:05 +0100

    Update generate.sh

--- gen/generate.sh
+++ gen/generate.sh
@@ -3,10 +3,10 @@
 # Determine run parameters
 jo=MC15.999999.MadGraphPythia8EvtGen_NNPDF23LO_DM.py
 ecmEnergy=13000. 
-maxEvents=10000
+maxEvents=1000
 skipEvents=0
 randomSeed=123456 
-outputEVNTFile=EVNT.HepMC
+outputEVNTFile=EVNT.pool.root
 
 
 workingDir=`pwd`
